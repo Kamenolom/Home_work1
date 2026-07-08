2b4685f5bbeb3dd7715805c3b78ae462e869f67f
Kamenolom <iliristvasilik8553@gmail.com>
2026-07-09 00:33:42 +0200

    Home work changes v2

---
+++ home_work3.py
@@ -0,0 +1,18 @@
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter second number:"))
+operation = input("Enter operation: +,-,*,/ is:")
+if operation == "+":
+    result = number1 + number2
+elif operation == "-":
+    result = number1 - number2
+elif operation == "*":
+    result = number1 * number2
+elif operation == "/":
+    if number2 !=0:
+        result = number1 / number2
+    else:
+        result  = "Enter correct number not '0'"
+    print("Your result is:" ,result)
+
+
+
