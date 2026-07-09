57dbcd35eebb970d86963b9bdcaa9302eb3ff15c
Kamenolom <iliristvasilik8553@gmail.com>
2026-07-09 22:31:53 +0200

    ooooohh yeeees

---
+++ hw_change_elements.py
@@ -0,0 +1,45 @@
+# list = [1,2,3,4,5,6,7,8]
+# middle = (len(list)) // 2
+# left = list[:middle]
+# right = list[middle:]
+# result = [left, right]
+# print(result)
+
+# list = [1,2,3,4,5]
+# middle = (len(list) + 1) //2
+# left = list[:middle]
+# right = list[middle:]
+# result = [left , right]
+# print(result)
+
+# list = [1,2,3]
+# middle = (len(list) +1) //2
+# left = list[:middle]
+# right = list[middle:]
+# result = [left , right]
+# print(result)
+
+# list = [1]
+# number1 = []
+# result = [list , number1]
+# print(result)
+
+# list = []
+# list2 =[]
+# result = [list , list2]
+# print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
