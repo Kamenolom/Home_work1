8c96a44588379ea25c80ff7ade4065e9e20c9ef5
Kamenolom <iliristvasilik8553@gmail.com>
2026-07-09 23:21:43 +0200

    ooooohh yeeees

---
+++ home_work3_parth3.py
@@ -0,0 +1,27 @@
+# numbers = [1,3,4,5,9]
+# last = numbers.pop()
+# numbers.insert(0,last)
+# print(numbers)
+
+# numbers = [1]
+# operation = numbers.pop()
+# numbers.insert(0, operation)
+# print(numbers)
+
+# numbers = [1,45,12,20,14,28,10]
+# magic = numbers.pop()
+# numbers.insert(0,magic)
+# print(numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
