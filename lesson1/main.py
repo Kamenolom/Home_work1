27d6a35c91041dced776eb4788ae769e878f13da
Kamenolom <iliristvasilik8553@gmail.com>
2026-07-12 01:11:23 +0200

    add lesson 1 and lesson 3 homework

---
+++ lesson1/main.py
@@ -0,0 +1,134 @@
+
+
+# numbers = [5, 2, 8, 1, 3]
+# # numbers.sort()
+# # numbers.reverse()
+# # numbers.append(10)
+# # print(numbers)
+# print(min(numbers))
+# print(max(numbers))
+
+# number = 1
+# while number <= 10:
+#     print(number)
+#     number +=1
+
+# total = 0
+# for number in range(1, 5):
+#     total = total + number
+# print(total)
+
+# numbers = [5, 10, 15, 20]
+# for number in numbers:
+#     print(number)
+
+# numbers = [3, 7, 2, 10, 5]
+# total = 0
+# for number in numbers:
+#     total = total + number
+#     print(total)
+
+
+# numbers = [5, 15, 2, 30, 7, 20]
+# for number in numbers:
+#     if number > 10:
+#         print(number)
+
+# numbers = [4, 7, 10, 13, 20, 25]
+# for number in numbers:
+#     if number % 2 == 0:
+#         print(number)
+
+# numbers = [3, 8, 15, 22, 7, 30]
+# for number in numbers:
+#     if number < 10:
+#         print(number)
+
+# numbers = [5, 12, 3, 20, 8]
+# for number in numbers:
+#     if number >= 10:
+#         print("Big number")
+#     else:
+#         print("Small number")
+
+# numbers = [1, 2, 3, 4, 5, 6]
+# num_numbers = []
+# for number in numbers:
+#     if number % 2 == 0:
+#         num_numbers.append(number)
+# print(num_numbers)
+
+# numbers = []
+# for number in range(1, 51):
+#     if number % 5 ==0:
+#         numbers.append(number)
+# print(numbers)
+
+# numbers = [4, 7, 10, 15, 22, 30]
+# count = 0
+# for number in numbers:
+#     if number > 15:
+#         count += 1
+# print(count)
+
+# numbers = []
+# count = 0
+# for number in range(1, 31):
+#     if number %3 == 0:
+#         numbers.append(number)
+#         count = count + 1
+# print(numbers)
+# print(count)
+
+# for zyzz in range(1, 4):
+#     for zxc in range(1, 4):
+#         print(zyzz,end=" ")
+#     print()
+
+# for row in range(1, 4):
+#     for col in range(1, 4):
+#         print(5, end=" ")
+#     print()
+
+# import random
+# length = random.randint(3, 10)
+# numbers = []
+# for i in range(length):
+#     numbers.append(random.randint(3, 10))
+# new_numbers = [numbers[0], numbers[2], numbers[-2]]
+# print(numbers,new_numbers)
+
+
+# import random
+# length = random.randint(5, 8)
+# numbers = []
+# for i in range(length):
+#     numbers.append(random.randint(1, 20))
+# new_numbers = [numbers[0], numbers[1], numbers[-1]]
+# print(numbers, new_numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
