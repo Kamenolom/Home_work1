27efd5445d162fe25d0e9bf5df88f178021c6dbf
Kamenolom <iliristvasilik8553@gmail.com>
2026-07-12 00:59:56 +0200

    impossible_home_work

---
+++ home_work4.1.py
@@ -0,0 +1,30 @@
+# import random
+# length = random.randint(3, 10)
+# numbers = []
+# for i in range(length):
+#     numbers.append(random.randint(3, 10))
+# new_numbers = [numbers [0], numbers[2], numbers[-2]]
+# print(numbers, new_numbers)
+
+# import random
+# length = 4
+# numbers = []
+# for i in range(length):
+#     numbers.append(random.randint(1, 4))
+# new_numbers = [numbers[0], numbers[2], numbers[-2]]
+# print(numbers, new_numbers)
+
+# import random
+# length = 3
+# numbers = []
+# for i in range(length):
+#     numbers.append(random.randint(1, 3))
+# new_numbers = [numbers[0], numbers[2], numbers[-2]]
+# print(numbers, new_numbers)
+
+
+
+
+
+
+
