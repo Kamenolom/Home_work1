d4b059ed14991e1ee018a5f0d6c15bff70cfa397
Kamenolom <iliristvasilik8553@gmail.com>
2026-07-12 23:12:21 +0200

    impossible_home_work

---
+++ home_work4.2.py
@@ -0,0 +1,41 @@
+# numbers = [1, 0 ,2, 12, 0, 3]
+# result = []
+# for number in numbers:
+#     if number != 0:
+#         result.append(number)
+# for number in numbers:
+#     if number == 0:
+#         result.append(number)
+# print(numbers, result)
+
+# numbers = [0]
+# result = []
+# for number in numbers:
+#     if number == 0:
+#         result.append(number)
+# print(numbers, result)
+
+# numbers = [1, 0, 13, 0, 0, 0, 5]
+# result = []
+# for number in numbers:
+#     if number != 0:
+#         result.append(number)
+# for number in numbers:
+#     if number == 0:
+#         result.append(number)
+# print(numbers, result)
+
+# numbers = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+# result = []
+# for number in numbers:
+#     if number != 0:
+#         result.append(number)
+# for number in numbers:
+#     if number == 0:
+#         result.append(number)
+# print(numbers, result)
+
+
+
+
+
