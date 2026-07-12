e314c59a185303fe4504b0b19d967c9231d02120
Kamenolom <iliristvasilik8553@gmail.com>
2026-07-13 00:14:21 +0200

    impossible_home_work

---
+++ lesson4/home_work4.3.py
@@ -0,0 +1,26 @@
+# numbers = [2, 4, 5, 1, 6 ,2]
+# total = 0
+# for i in range(0, 6, 2):
+#   total += numbers[i]
+# result = total * numbers[-1]
+# print(result)
+
+# numbers = [6]
+# total = 0
+# for i in range(0, len(numbers), 2):
+#     total = total + numbers[i]
+# result = total * numbers[-1]
+# print(result)
+
+# numbers =[]
+# if not numbers:
+#     result = 0
+# print(numbers, result)
+
+
+
+
+
+
+
+
