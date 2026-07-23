d88d103af8eb7210ef62f2fc7b5c19c297270635
Kamenolom <iliristvasilik8553@gmail.com>
2026-07-23 07:19:50 +0200

    good job!

---
+++ lesson7/hw7.2.py
@@ -0,0 +1,10 @@
+def correct_sentence(text):
+    text = text[0].upper() + text[1:]
+    if text[-1] != ".":
+        text += "."
+    return text
+print(correct_sentence("greetings, friends"))
+print(correct_sentence("hello"))
+print(correct_sentence("Greetings. Friends"))
+print(correct_sentence("Greetings, friends."))
+print(correct_sentence("greetings, friends."))
